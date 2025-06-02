96ad42705a07ea29d41d017ae93ce2b9369713b3
Dev-id-A <davidaranzana618@gmail.com>
2025-06-02 09:34:11 +0200

    axis displayed

--- script.js
+++ script.js
@@ -34,11 +34,39 @@
 
 //Description
         svg.append("text")
-            .text("1753 - 2015: base temperature 8.66 ℃")
+            .text(`1753 - 2015: base temperature ${data.baseTemperature}℃`)
             .attr("id", "description")
             .attr("y", padding/1.7)
             .attr("x", width/2.7)
             .style("font-size", "14px");
+
+//xScale
+        const xScale = d3.scaleLinear()
+                            .domain(d3.extent(data.monthlyVariance, d=>d.year))
+                            .range([padding, width - padding]);
+
+        const xAxis = d3.axisBottom(xScale)
+                            .tickValues(d3.range(1760, 2015, 10))
+                            .tickFormat(d3.format("d"));
+
+        svg.append("g")
+            .attr("id", "x-axis")
+            .attr("transform", `translate(0, ${height - padding*1.5})`)
+            .style("font-size", "7px")
+            .call(xAxis)
+    
+//yScale
+const months = ["December", "November", "October", "September", "August", "July", "June", "May", "April", "March", "February", "January" ]
+        const yScale = d3.scaleBand()
+                            .domain(months)
+                            .range([height - padding*1.5, padding])
+
+        const yAxis = d3.axisLeft(yScale);
+                            
+        svg.append("g")
+            .attr("id", "y-axis")
+            .attr("transform", `translate(${padding}, 0)`)
+            .call(yAxis);
 
 
         
